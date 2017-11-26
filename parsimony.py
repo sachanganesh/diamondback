662eecee6a48a4db655a704c23e2e977a9205bd1
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-11-26 18:38:40 -0500

    initial commit

---
+++ parsimony.py
@@ -0,0 +1,7 @@
+"""
+Parsimony Tree Improvement 2017
+By Sachandhan Ganesh
+
+General Usage:
+	parsimony.py -a <alignment> -n <newick_tree> -r <number_of_random_rearrangements> -p <probability>
+"""
