8ae425623a81737009a2e8ac6c98317fad7ca616
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-12-11 23:18:02 -0500

    include README and cosmetic updates

--- parsimony-submission/parsimony.py
+++ parsimony-submission/parsimony.py
@@ -1,7 +1,9 @@
 """
-Mossback: Parsimony Tree Search 2017
+Parsimony Tree Improvement 2017
 By Sachandhan Ganesh
 
+General Usage:
+	parsimony.py -a <alignment> -n <newick_tree> -r <number_of_random_rearrangements> -p <probability>
 """
 
 import argparse
